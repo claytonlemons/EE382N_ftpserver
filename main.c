6557393d46849d3c455494cee8fee095c03049e9
jlloyola <joseloylar@hotmail.com>
2015-10-29 20:09:43 -0500

    lwip Library Port
    Added the lwip library as a standalone module (i.e. without linking to
    the Stellaris folder). Updated the dependencies of the project to
    correctly point to these files.
    
    Increased the stack size from 512 to 1024 because the board was running
    out of memory. (Checked the ethernet example and they also use a stack
    of 1024)
    
    lm3s8962_startup_ccs.c: Linked interrupt handlers for the Systick and
    the ethernet module.
    
    Main: Added the following methods:
    DisplayIPAddress
    SysTickIntHandler
    lwIPHostTimerHandler
    ftp_init/ftp_accept: These methods will probably move to other file, but
    they were used as a quick test to see if everything was working after
    porting the library.

--- main.c
+++ main.c
@@ -1,7 +1,277 @@
 /*
  * main.c
  */
+#include <string.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_nvic.h"
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "utils/lwiplib.h"
+#include "utils/locator.h"
+#include "utils/ustdlib.h"
+#include "driverlib/flash.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/ethernet.h"
+#include "drivers/rit128x96x4.h"
+#include "driverlib/interrupt.h"
+
+#include "lwip/tcp.h"
+
+//#include "io.h"
+//#include "cgifuncs.h"
+
+//*****************************************************************************
+//
+// A set of flags.  The flag bits are defined as follows:
+//
+//     0 -> An indicator that a SysTick interrupt has occurred.
+//
+//*****************************************************************************
+#define FLAG_SYSTICK            0
+static volatile unsigned long g_ulFlags;
+
+//*****************************************************************************
+//
+// Defines for setting up the system clock.
+//
+//*****************************************************************************
+#define SYSTICKHZ               100
+#define SYSTICKMS               (1000 / SYSTICKHZ)
+#define SYSTICKUS               (1000000 / SYSTICKHZ)
+#define SYSTICKNS               (1000000000 / SYSTICKHZ)
+
+//*****************************************************************************
+//
+// Display an lwIP type IP Address.
+//
+//*****************************************************************************
+void
+DisplayIPAddress(unsigned long ipaddr, unsigned long ulCol,
+                 unsigned long ulRow)
+{
+    char pucBuf[16];
+    unsigned char *pucTemp = (unsigned char *)&ipaddr;
+
+    //
+    // Convert the IP Address into a string.
+    //
+    usprintf(pucBuf, "%d.%d.%d.%d", pucTemp[0], pucTemp[1], pucTemp[2],
+             pucTemp[3]);
+
+    //
+    // Display the string.
+    //
+    RIT128x96x4StringDraw(pucBuf, ulCol, ulRow, 15);
+}
+
+//*****************************************************************************
+//
+// The interrupt handler for the SysTick interrupt.
+//
+//*****************************************************************************
+void
+SysTickIntHandler(void)
+{
+    //
+    // Indicate that a SysTick interrupt has occurred.
+    //
+    HWREGBITW(&g_ulFlags, FLAG_SYSTICK) = 1;
+
+    //
+    // Call the lwIP timer handler.
+    //
+    lwIPTimer(SYSTICKMS);
+}
+
+//*****************************************************************************
+//
+// Required by lwIP library to support any host-related timer functions.
+//
+//*****************************************************************************
+void
+lwIPHostTimerHandler(void)
+{
+    static unsigned long ulLastIPAddress = 0;
+    unsigned long ulIPAddress;
+
+    ulIPAddress = lwIPLocalIPAddrGet();
+
+    //
+    // If IP Address has not yet been assigned, update the display accordingly
+    //
+    if(ulIPAddress == 0)
+    {
+        static int iColumn = 6;
+
+        //
+        // Update status bar on the display.
+        //
+        RIT128x96x4Enable(1000000);
+        if(iColumn < 12)
+        {
+            RIT128x96x4StringDraw(" >", 114, 24, 15);
+            RIT128x96x4StringDraw("< ", 0, 24, 15);
+            RIT128x96x4StringDraw("*",iColumn, 24, 7);
+        }
+        else
+        {
+            RIT128x96x4StringDraw(" *",iColumn - 6, 24, 7);
+        }
+
+        iColumn += 4;
+        if(iColumn > 114)
+        {
+            iColumn = 6;
+            RIT128x96x4StringDraw(" >", 114, 24, 15);
+        }
+        RIT128x96x4Disable();
+    }
+
+    //
+    // Check if IP address has changed, and display if it has.
+    //
+    else if(ulLastIPAddress != ulIPAddress)
+    {
+        ulLastIPAddress = ulIPAddress;
+        RIT128x96x4Enable(1000000);
+        RIT128x96x4StringDraw("                       ", 0, 16, 15);
+        RIT128x96x4StringDraw("                       ", 0, 24, 15);
+        RIT128x96x4StringDraw("IP:   ", 0, 16, 15);
+        RIT128x96x4StringDraw("MASK: ", 0, 24, 15);
+        RIT128x96x4StringDraw("GW:   ", 0, 32, 15);
+        DisplayIPAddress(ulIPAddress, 36, 16);
+        ulIPAddress = lwIPLocalNetMaskGet();
+        DisplayIPAddress(ulIPAddress, 36, 24);
+        ulIPAddress = lwIPLocalGWAddrGet();
+        DisplayIPAddress(ulIPAddress, 36, 32);
+        RIT128x96x4Disable();
+    }
+}
+
+static err_t
+ftp_accept(void *arg, struct tcp_pcb *pcb, err_t err)
+{
+  LWIP_UNUSED_ARG(arg);
+  LWIP_UNUSED_ARG(err);
+  RIT128x96x4Enable(1000000);
+  RIT128x96x4StringDraw("FTP_Accept Called!", 0, 80, 15);
+  RIT128x96x4Disable();
+
+  /* Tell TCP that this is the structure we wish to be passed for our
+     callbacks. */
+  //tcp_arg(pcb, hs);
+
+  /* Tell TCP that we wish to be informed of incoming data by a call
+     to the http_recv() function. */
+  //tcp_recv(pcb, http_recv);
+
+  //tcp_err(pcb, conn_err);
+
+  //tcp_poll(pcb, http_poll, 4);
+  return ERR_OK;
+}
+//////////////////////////////////////////////////////////////////
+void
+ftp_init(void)
+{
+  struct tcp_pcb *pcb;
+
+  // Create new TCP control block
+  pcb = tcp_new();
+  // Bind the control block to any IP address and port 21
+  tcp_bind(pcb, IP_ADDR_ANY, 21);
+  // Tell TCP to listen to this port/control block
+  pcb = tcp_listen(pcb);
+  // Used to specify the function that should be called when
+  // a listening connection is established
+  tcp_accept(pcb, ftp_accept);
+}
+
 int main(void) {
-	
-	return 0;
+    unsigned long ulUser0, ulUser1;
+    unsigned char pucMACArray[8];
+    //
+    // Set the clocking to run directly from the crystal.
+    //
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_8MHZ);
+
+    //
+    // Initialize the OLED display.
+    //
+    RIT128x96x4Init(1000000);
+    RIT128x96x4StringDraw("FTP Server", 0, 0, 15);
+
+    //
+    // Enable and Reset the Ethernet Controller.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ETH);
+    SysCtlPeripheralReset(SYSCTL_PERIPH_ETH);
+
+    //
+    // Configure SysTick for a periodic interrupt.
+    //
+    SysTickPeriodSet(SysCtlClockGet() / SYSTICKHZ);
+    SysTickEnable();
+    SysTickIntEnable();
+
+    //
+    // Enable processor interrupts.
+    //
+    IntMasterEnable();
+
+    //
+    // Configure the hardware MAC address for Ethernet Controller filtering of
+    // incoming packets.
+    //
+    // For the LM3S6965 Evaluation Kit, the MAC address will be stored in the
+    // non-volatile USER0 and USER1 registers.  These registers can be read
+    // using the FlashUserGet function, as illustrated below.
+    //
+    FlashUserGet(&ulUser0, &ulUser1);
+    if((ulUser0 == 0xffffffff) || (ulUser1 == 0xffffffff))
+    {
+        //
+        // We should never get here.  This is an error if the MAC address
+        // has not been programmed into the device.  Exit the program.
+        //
+        RIT128x96x4StringDraw("MAC Address", 0, 16, 15);
+        RIT128x96x4StringDraw("Not Programmed!", 0, 24, 15);
+        while(1);
+    }
+
+    //
+    // Convert the 24/24 split MAC address from NV ram into a 32/16 split
+    // MAC address needed to program the hardware registers, then program
+    // the MAC address into the Ethernet Controller registers.
+    //
+    pucMACArray[0] = ((ulUser0 >>  0) & 0xff);
+    pucMACArray[1] = ((ulUser0 >>  8) & 0xff);
+    pucMACArray[2] = ((ulUser0 >> 16) & 0xff);
+    pucMACArray[3] = ((ulUser1 >>  0) & 0xff);
+    pucMACArray[4] = ((ulUser1 >>  8) & 0xff);
+    pucMACArray[5] = ((ulUser1 >> 16) & 0xff);
+
+    //
+    // Initialze the lwIP library, using DHCP.
+    //
+    lwIPInit(pucMACArray, 0, 0, 0, IPADDR_USE_DHCP);
+
+    //
+    // Setup the device locator service.
+    //
+    LocatorInit();
+    LocatorMACAddrSet(pucMACArray);
+    LocatorAppTitleSet("FTP Server");
+
+    // Initialize a sample FTP server.
+    ftp_init();
+
+    //
+    // Loop forever.  All the work is done in interrupt handlers.
+    //
+    while(1)
+    {
+    }
 }
